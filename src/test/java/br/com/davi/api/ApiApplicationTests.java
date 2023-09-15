7b9c5087226c09548c8f5301a209946c5406436e
DaviFarias7 <davic.farias7@gmail.com>
2023-09-14 21:38:34 -0300

    projeto iniciado

---
+++ src/test/java/br/com/davi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.davi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
