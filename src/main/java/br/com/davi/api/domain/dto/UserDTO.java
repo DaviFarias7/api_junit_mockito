88de78fe97a32b9f1e1771a4a5ecf099023b3e21
DaviFarias7 <davic.farias7@gmail.com>
2023-09-15 15:02:26 -0300

    classe user dto

---
+++ src/main/java/br/com/davi/api/domain/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package br.com.davi.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
