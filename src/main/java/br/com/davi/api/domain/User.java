33d65fd39edc73ccbfa42cd504ab1a290c901c5b
DaviFarias7 <davic.farias7@gmail.com>
2023-09-14 21:53:02 -0300

    classe User

---
+++ src/main/java/br/com/davi/api/domain/User.java
@@ -0,0 +1,21 @@
+package br.com.davi.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
