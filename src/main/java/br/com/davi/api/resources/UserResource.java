7f3977b4af81b3b6a39d2fc448308e3a96bb761c
DaviFarias7 <davic.farias7@gmail.com>
2023-09-14 22:32:09 -0300

    classe user resource findById

--- src/main/java/br/com/davi/api/resources/UserResource.java
+++ src/main/java/br/com/davi/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.davi.api.resources;
 
 import br.com.davi.api.domain.User;
+import br.com.davi.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1,"Davi", "davi@gmail.com", "123"));
+        return ResponseEntity.ok().body(service.findByid(id));
     }
 }
