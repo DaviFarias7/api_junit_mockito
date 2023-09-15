2f69802f031f7478fb165a2cb9ec3b945d88ba4e
DaviFarias7 <davic.farias7@gmail.com>
2023-09-15 15:30:05 -0300

    endpoint findAll para user

--- src/main/java/br/com/davi/api/services/UserService.java
+++ src/main/java/br/com/davi/api/services/UserService.java
@@ -3,6 +3,9 @@ package br.com.davi.api.services;
 
 import br.com.davi.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User findByid(Integer id);
+    List<User>findAll();
 }
--- src/main/java/br/com/davi/api/services/impl/UserServiceimpl.java
+++ src/main/java/br/com/davi/api/services/impl/UserServiceimpl.java
@@ -7,6 +7,7 @@ import br.com.davi.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,9 @@ public class UserServiceimpl implements UserService {
     public User findByid(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<User>findAll(){
+        return repository.findAll();
     }
 }
