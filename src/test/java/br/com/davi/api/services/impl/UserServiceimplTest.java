81eb08be620c36a7d7b75163622bd02b8887615d
DaviFarias7 <davic.farias7@gmail.com>
2023-09-18 21:59:31 -0300

    teste para a classe user service metodo create

--- src/test/java/br/com/davi/api/services/impl/UserServiceimplTest.java
+++ src/test/java/br/com/davi/api/services/impl/UserServiceimplTest.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -90,7 +91,18 @@ class UserServiceimplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+
     }
 
     @Test
